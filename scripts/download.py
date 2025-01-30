917dd109303426338c0c2d3f1970ca1bcae230a2
Norema9 <97368427+Norema9@users.noreply.github.com>
2025-01-30 23:28:07 +0100

    added download function in the utils for downloading audios from youtub playlist link

--- scripts/download.py
+++ scripts/download.py
@@ -1,13 +1,22 @@
 import os
+import sys
+from typing import Dict
 import logging
+from pathlib import Path
 
+working_directory = Path(__file__).parent.parent
+os.chdir(working_directory)
+sys.path.append(str(working_directory))
+
+# Import from the current project
 from src.data import download
 from src import utils
 
-
 if __name__ == "__main__":
-    tv_shows = utils.read_yaml("configs/config.yaml")["tv-show"]
+    tv_shows:Dict = utils.read_yaml("configs/config.yaml")["tv-show"]
     os.makedirs("data", exist_ok=True)
-    for name, url in tv_shows.items():
-        print(url)
-        download.download_playlist(name, url, "audio")
+    for play_list, url in tv_shows.items():
+        download(url, play_list)
+
+
+
